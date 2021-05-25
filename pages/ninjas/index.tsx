5b199ec9fa1f678eeeb3bfd66f1bf707ca0daed0
Dave Inoc <michael828inoc@gmail.com>
2021-05-25 15:29:15 +0800

    added data

--- pages/ninjas/index.tsx
+++ pages/ninjas/index.tsx
@@ -2,13 +2,42 @@ import React, { FC } from "react";
 
 import Meta from "../../components/Meta";
 
-const index: FC = () => {
+import styles from "../../styles/Ninjas.module.css";
+
+type Props = {
+  ninjas: NinjasInfo[];
+};
+
+type NinjasInfo = {
+  id: number;
+  name: string;
+  username: string;
+  website: string;
+};
+
+const index: FC<Props> = ({ ninjas }: Props) => {
   return (
     <>
       <Meta title="Vercel | Ninjas" />
-      <h1>NINJA LISTING</h1>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </>
   );
 };
 
 export default index;
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
