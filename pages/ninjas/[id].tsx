153d5b4df4178e5d538293f5809d6a6675c05d43
Dave Inoc <michael828inoc@gmail.com>
2021-05-25 15:38:00 +0800

    static paths

--- pages/ninjas/[id].tsx
+++ pages/ninjas/[id].tsx
@@ -9,3 +9,19 @@ const Details = () => {
 };
 
 export default Details;
+
+export const getStaticPaths = async () => {
+  const res = await fetch("http://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
