b87c1fdc74ac97f5b09e33d771e115b6373429f8
Dave Inoc <michael828inoc@gmail.com>
2021-05-25 15:43:56 +0800

    added go back

--- pages/ninjas/[id].tsx
+++ pages/ninjas/[id].tsx
@@ -1,14 +1,33 @@
+import { useRouter } from "next/router";
 import React from "react";
 
-const Details = () => {
+const Details = ({ ninja }) => {
+  const router = useRouter();
+
   return (
     <div>
-      <h1>Details Page</h1>
+      <a onClick={() => router.back()}>Go Back</a>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };
 
 export default Details;
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+
+  const res = await fetch(`http://jsonplaceholder.typicode.com/users/${id}`);
+
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
 
 export const getStaticPaths = async () => {
   const res = await fetch("http://jsonplaceholder.typicode.com/users");
