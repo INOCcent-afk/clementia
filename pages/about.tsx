c79c7c2bd796ac07b7e14d3c20e346c7598cdb38
Dave Inoc <michael828inoc@gmail.com>
2021-05-25 15:08:15 +0800

    added 404 page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,16 @@ const about = () => {
   return (
     <div>
       <h1>ABOUT</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente nemo
+        cupiditate maxime! Perferendis adipisci nihil atque molestias voluptates
+        omnis natus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente nemo
+        cupiditate maxime! Perferendis adipisci nihil atque molestias voluptates
+        omnis natus!
+      </p>
     </div>
   );
 };
